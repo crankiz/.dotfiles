374d964dab89dd9f7eb9812bde282c60bb9d0c7b
crankiz <svedberg.tobias@gmail.com>
2020-01-20 16:24:33 +0100

    Add gnu stow support

---
+++ stow_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+readarray -d '' array < <(find ~/.dotfiles -mindepth 1 -maxdepth 1 -type d -printf "%f\n")
+
+
+for i in "${array[@]}"
+do :
+    if stow --no $i ; then
+        echo "Create symlinks for $i"
+    else
+        echo "Conflict occurd for $i do you want to override"
+
+    fi
+done
+
+
