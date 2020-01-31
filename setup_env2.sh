ae04db6de15fdc5c6a915850ba7efac835f87b2c
Tobias Svedberg <svedberg.tobias@gmail.com>
2020-01-31 14:38:17 +0100

    test 666

--- setup_env2.sh
+++ setup_env2.sh
@@ -10,12 +10,6 @@ do
     command -v $pkg &> /dev/null || sudo pacman --noconfirm -S $pkg || sudo apt install -y $pkg
 done
 
-# Install zgen
-echo -e "Installing zgen\n"
-if git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"; then
-	echo -e "Installed zgen\n"
-fi
-
 # Git clone my dotfiles
 echo -e "Git clone my dotfiles\n"
 if git clone https://github.com/crankiz/.dotfiles.git ${HOME}/.dotfiles; then :
@@ -23,29 +17,52 @@ else
     cd ${HOME}/.dotfiles && git pull && cd ${HOME}
 fi
 
-# Install plugins and themes
-
-
-# INSTALL FONTS
-if git clone https://github.com/powerline/fonts.git ${HOME}/powerline_fonts; then :
-else
-	cd ${HOME}/powerline_fonts && git pull 
+# Create .zsh_plugins directory
+if [ ! -d "${HOME}/.zsh_plugins" ]; then
+  mkdir ${HOME}/.zsh_plugins
 fi
 
-if ${HOME}/powerline_fonts/install.sh && rm -rf ${HOME}/powerline_fonts; then
+# Install plugins and themes
+echo -e "Git clone powerlevel10k\n"
+if git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${HOME}/.zsh_plugins/powerlevel10k; then :
+else
+	cd ${HOME}/.zsh_plugins/powerlevel10k && git pull && cd ${HOME}
+fi
+echo -e "Git clone zsh-autosuggestions\n"
+if git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ${HOME}/.zsh_plugins/zsh-autosuggestions; then :
+else
+	cd ${HOME}/.zsh_plugins/zsh-autosuggestions && git pull && cd ${HOME}
+fi
+echo -e "Git clone zsh-completions\n"
+if git clone --depth=1 https://github.com/zsh-users/zsh-completions.git ${HOME}/.zsh_plugins/zsh-completions; then :
+else
+	cd ${HOME}/.zsh_plugins/zsh-completions && git pull && cd ${HOME}
+fi
+echo -e "Git clone zsh-syntax-highlighting\n"
+if git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${HOME}/.zsh_plugins/zsh-syntax-highlighting; then :
+else
+	cd ${HOME}/.zsh_plugins/zsh-syntax-highlighting && git pull && cd ${HOME}
+fi
+echo -e "Git clone powerline_fonts\n"
+if git clone --depth=1 https://github.com/powerline/fonts.git ${HOME}/.zsh_plugins/powerline_fonts; then :
+else
+	cd ${HOME}/.zsh_plugins/powerline_fonts && git pull && cd ${HOME}
+fi
+
+if ${HOME}/.zsh_plugins/powerline_fonts && rm -rf ${HOME}/.zsh_plugins/powerline_fonts; then
 	echo -e "\npowerline_fonts Installed\n"
 else
 	echo -e "\npowerline_fonts Installation Failed\n"
 fi
 
-curl -s https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Regular.ttf -o ${HOME}/.dotfiles/fonts
-curl -s https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold.ttf -o ${HOME}/.dotfiles/fonts
-curl -s https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Italic.ttf -o ${HOME}/.dotfiles/fonts
-curl -s https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold%20Italic.ttf -o ${HOME}/.dotfiles/fonts
+# curl -s https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Regular.ttf -o ${HOME}/.dotfiles/fonts
+# curl -s https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold.ttf -o ${HOME}/.dotfiles/fonts
+# curl -s https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Italic.ttf -o ${HOME}/.dotfiles/fonts
+# curl -s https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold%20Italic.ttf -o ${HOME}/.dotfiles/fonts
 
 
+touch ${HOME}/.zshrc
 echo -e "\nSudo access is needed to change default shell\n"
-
 if chsh -s $(which zsh); then
     cd ${HOME}/.dotfiles
     stow --adopt bash
@@ -53,10 +70,8 @@ if chsh -s $(which zsh); then
     stow --adopt zsh
     stow --adopt p10k
     cd ${HOME}
-    source ${HOME}/.zshrc
-	exec $(which zsh) -i 
 	echo -e "Installation Successful, exit terminal and enter a new session"
 else
 	echo -e "Something is wrong"
 fi
-exit
+exec $(which zsh) -i 
